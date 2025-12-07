97e38aad15b694138f60614a247e071ba6500ea6
SANDHYA <24P31F00D4@acet.ac.in>
2025-12-07 21:21:37 +0530

    median of an array

---
+++ Median.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class Median {
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of elements of an array: ");
+        int num = sc.nextInt();
+        System.out.println("Enter "+num+" numbers to array : ");
+        int arr[]=new int[num];
+        for(int i=0; i<num; i++){
+            arr[i]=sc.nextInt();
+        }
+        Arrays.sort(arr);
+        double median;
+        if(num%2==0){
+                    int val = num/2;
+            median = (arr[val-1]+arr[val])/2.0;
+        }
+        else{
+            median = arr[num/2];
+        }
+        System.out.println("Median is: "+median);
+
+        sc.close();
+    }
+}
