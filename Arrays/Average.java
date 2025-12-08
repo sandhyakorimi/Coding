dffa2a98c5bc7375daf90ca8c9a2db1ba00303e6
SANDHYA <24P31F00D4@acet.ac.in>
2025-12-08 21:59:16 +0530

    Average.java

---
+++ Arrays/Average.java
@@ -0,0 +1,23 @@
+package Arrays;
+import java.util.Scanner;
+
+public class Average {
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of elements of an array: ");
+        int num = sc.nextInt();
+        System.out.println("Enter "+num+" numbers to array : ");
+        int arr[]=new int[num];
+        for(int i=0; i<num; i++){
+            arr[i]=sc.nextInt();
+        }
+        int count=0;
+        for(int i:arr){
+            count+=i;
+        }
+        int avg=count/num;
+        System.out.println("Average: "+avg);
+
+        sc.close();
+    }
+}
