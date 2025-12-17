9f415a51cc1c99c1deb20e8e503e67b5ef67b2b2
SANDHYA <24P31F00D4@acet.ac.in>
2025-12-17 23:08:09 +0530

    Concatenation of arrays

---
+++ Arrays_And_Hashing/ContainsDuplicates.java
@@ -0,0 +1,13 @@
+package Arrays_And_Hashing;
+import java.util.Arrays;
+public class ContainsDuplicates {
+    public boolean ContainsDuplicates(int nums[]){
+        Arrays.sort(nums);
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]==nums[i++]){
+                return true;
+            }
+        }
+        return false;
+    }
+}
