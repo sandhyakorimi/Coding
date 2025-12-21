efb501ff806be69f32675c178d013ff41fa4c2ae
SANDHYA <24P31F00D4@acet.ac.in>
2025-12-21 23:59:47 +0530

    majority

--- Arrays_And_Hashing/Majority.java
+++ Arrays_And_Hashing/Majority.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 
 public class Majority{
-    public int majority(int[] nums){
+    public int majority(int[] nums){ 
         Arrays.sort(nums); 
         return nums[nums.length/2];          
     }       
