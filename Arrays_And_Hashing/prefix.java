c262bfe31f0b25a2d6c95972863e9167cb030bac
SANDHYA <24P31F00D4@acet.ac.in>
2025-12-22 11:54:09 +0530

    Arrays and hashing

--- Arrays_And_Hashing/prefix.java
+++ Arrays_And_Hashing/prefix.java
@@ -10,3 +10,7 @@ public class prefix {
         return strs[0];
     }
 }
+// In this problem, I compare all the strings column by column by taking the first string as the reference.
+// At each index, I check whether the character is common across all strings.
+// If a mismatch occurs or any string ends, I return the prefix up to the previous index.
+// If no mismatch occurs, I return the reference string as the longest common prefix.
