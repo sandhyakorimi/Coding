7a3d7ed1cb67940e4d992c91873e3e84c081a246
SANDHYA <24P31F00D4@acet.ac.in>
2025-12-24 22:00:24 +0530

    Remove element

--- Arrays_And_Hashing/RemoveElement.java
+++ Arrays_And_Hashing/RemoveElement.java
@@ -12,4 +12,4 @@ public class RemoveElement {
 // “This method removes a given value in-place using two pointers.
 // I iterate through the array and copy only the elements not equal to the given value to the front.
 // I return k, which represents the number of valid elements.
-// Time complexity is O(n) and space complexity is O(1).”
+// Time complexity is O(n) and space complexity is O(1).    
