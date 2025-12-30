4b4d32bbe2d8b872c74a77024a25ce105f71c6c4
SANDHYA <24P31F00D4@acet.ac.in>
2025-12-30 20:31:26 +0530

    First missing positive number of an array

--- Arrays_And_Hashing/FirstMissingPositive.java
+++ Arrays_And_Hashing/FirstMissingPositive.java
@@ -22,3 +22,8 @@ public class FirstMissingPositive {
         return n+1;
     }
 }
+// “In this problem, we find the first missing positive number using cyclic sort. We try to place each positive
+//  number x at its correct position x - 1. If a number is negative, greater than the array length, or already 
+//  in the correct position, we skip it. Otherwise, we swap it to its correct index. After rearranging, we scan
+//   the array again. If at any index i the value is not i + 1, then i + 1 is the missing positive number. If all
+//    positions are correct, the answer is n + 1. This solution works in O(n) time and O(1) extra space.”
