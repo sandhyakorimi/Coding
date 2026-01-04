cade7ff7a19bacfa7b9e4b67587bf1475079d4d0
SANDHYA <24P31F00D4@acet.ac.in>
2026-01-04 22:05:12 +0530

    best time to buy and sell

---
+++ Arrays_And_Hashing/BestTimetoBuyAndSell2.java
@@ -0,0 +1,11 @@
+public class BestTimetoBuyAndSell2{
+    public int BestTimetoBuyAndSell(int[] prices){
+        int profit =0;
+        for(int i=1; i<prices.length; i++){
+            if(prices[i]>prices[i-1]){
+                profit += (prices[i]- prices[i-1]);
+             }
+        }
+        return profit;
+    }
+}
