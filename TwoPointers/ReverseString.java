6782986aff9076e0ade8b80365b5a27a48782277
SANDHYA <24P31F00D4@acet.ac.in>
2026-01-06 21:40:46 +0530

    reverse string

---
+++ TwoPointers/ReverseString.java
@@ -0,0 +1,14 @@
+package TwoPointers;
+
+public class ReverseString {
+    public void Revstr(char[] s){
+        int i=0, r=s.length-1;
+        while(i<r){
+            char temp = s[i];
+            s[i] = s[r];
+            s[r] = temp;
+            i++;
+            r--;
+        }
+    }
+}
