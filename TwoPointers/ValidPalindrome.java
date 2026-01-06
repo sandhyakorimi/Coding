26985ffa849b24a04716e1da896afd558f4e74e6
SANDHYA <24P31F00D4@acet.ac.in>
2026-01-06 21:51:59 +0530

    valid palindrome

---
+++ TwoPointers/ValidPalindrome.java
@@ -0,0 +1,21 @@
+package TwoPointers;
+
+public class ValidPalindrome {
+    public boolean palindromeOrNot(String s){
+        int l =0, r = s.length();
+        while(l<r){
+            while(l<r && !alphaNumeric(s.charAt(l))){ l++; }
+            while(r>l && !alphaNumeric(s.charAt(r))){ r--;}
+            if(s.charAt(l)!=s.charAt(r)){
+                return false;
+            }
+            l++; r--;
+        }
+        return true;
+    }
+        public boolean alphaNumeric(char c){
+             return (c>='A' && c<='Z' || 
+                c>='a' && c<='z' || 
+                c>='0' && c<='9');
+        }
+}
