a52801d716e8f22a9f7b02e8a22d459f6b29bc2e
SANDHYA <24P31F00D4@acet.ac.in>
2026-01-12 21:46:06 +0530

    best time to buy and sell

---
+++ SlidingWindow/BestTimeToBuyAndSell.java
@@ -0,0 +1,13 @@
+package SlidingWindow;
+
+public class BestTimeToBuyAndSell {
+    public int BestTimeToBuyAndSell1(int[] prices){
+        int max=0;
+        int min = prices[0];
+        for(int i: prices){
+            max=Math.max(max, i-min);
+            min = Math.min(min, i);
+        }
+        return max;
+    }
+}
