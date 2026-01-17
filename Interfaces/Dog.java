049258fef86c3631f7535bda8650596a41c79f54
SANDHYA <24P31F00D4@acet.ac.in>
2026-01-17 22:17:38 +0530

    object

--- Interfaces/Dog.java
+++ Interfaces/Dog.java
@@ -3,7 +3,7 @@ public class Dog extends Animal{
     public static void main(String[] args) {
         Animal dog= new Dog();
         dog.eat();
-        dog.think();
+        dog.think(); 
         dog.Decide(); 
     } 
 }
