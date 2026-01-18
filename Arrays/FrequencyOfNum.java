edad2f817779bcd403812117b8f9b2d817b3ac14
SANDHYA <24P31F00D4@acet.ac.in>
2026-01-18 21:32:07 +0530

    frequeny of num

---
+++ Arrays/FrequencyOfNum.java
@@ -0,0 +1,27 @@
+package Arrays;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class FrequencyOfNum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of elements of an array: ");
+        int num = sc.nextInt();
+        System.out.println("Enter "+num+" numbers to array : ");
+        int arr[]=new int[num];
+        for(int i=0; i<num; i++){
+            arr[i]=sc.nextInt(); 
+        }  
+        Arrays.sort(arr);  
+        int count =1;
+        for(int j=0;j<arr.length;j++){
+            if(j<arr.length-1&&arr[j]==arr[j+1]){
+                count++; 
+            }else{
+            System.out.println("Frequency of "+arr[j]+" is :"+count);
+            count=1; 
+            }
+        }
+        sc.close();
+    }
+}
