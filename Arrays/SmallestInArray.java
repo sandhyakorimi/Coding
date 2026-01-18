9e82ae5f3aebd7f2b05fb3f09600977d7a6aba0f
SANDHYA <24P31F00D4@acet.ac.in>
2026-01-18 21:31:30 +0530

    smallest in array

---
+++ Arrays/SmallestInArray.java
@@ -0,0 +1,25 @@
+package Arrays;
+import java.util.Scanner;
+
+public class SmallestInArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter number of elements in an array: ");
+        int num = sc.nextInt();
+        System.out.println("Enter "+num+" numbers to Array");
+        int arr[]= new int[num];
+        for(int i=0; i<num; i++){
+           arr[i]  = sc.nextInt();
+            
+        }
+        System.out.println("Smallest number in the array is : ");
+        int less = arr[0];
+        for(int j=1; j<arr.length; j++){
+            if(arr[j]<less){
+                less=arr[j];
+            }
+        } 
+        System.out.print(less);
+        sc.close(); 
+    }
+}
