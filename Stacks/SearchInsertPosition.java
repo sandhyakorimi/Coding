f14c93cfc272fd50682569dbcf225feaff34b1eb
SANDHYA <24P31F00D4@acet.ac.in>
2026-01-22 21:48:58 +0530

    search insert position

--- Stacks/SearchInsertPosition.java
+++ Stacks/SearchInsertPosition.java
@@ -9,7 +9,7 @@ public class SearchInsertPosition {
             else if(nums[mid]>target){
                 res=mid;
                 r=mid-1;
-            }else{l=mid+1;}
+            }else{l=mid+1;} 
         }
         return res; 
     }
