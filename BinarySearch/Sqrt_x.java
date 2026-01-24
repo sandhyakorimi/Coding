bfe12e04f1a0da217f6de4aedb0dd3ec426a9ecf
SANDHYA <24P31F00D4@acet.ac.in>
2026-01-24 23:16:38 +0530

    square root of x

---
+++ BinarySearch/Sqrt_x.java
@@ -0,0 +1,17 @@
+public class Sqrt_x {
+    public int Square_x(int x){
+        int l=0, r=x, res=0;
+        while(l<=r){
+            int m=l+(r-l)/2;
+            if((long)m*m>x){
+                r=m-1;
+            }else if((long)m*m<x){
+                l=m+1;
+                res=m;
+            }else{
+                return m;
+            }
+        }
+        return res;
+    }
+}
