bc5c1b3183ab6d42bc1b66ef7070111b0fbc9c6b
SANDHYA <24P31F00D4@acet.ac.in>
2026-01-25 21:58:07 +0530

    base ball game using operators

--- Stacks/BaseballGame.java
+++ Stacks/BaseballGame.java
@@ -24,3 +24,16 @@ public class BaseballGame {
         return sum;
     }
 }
+// “I solved this problem using a stack.
+// I iterate over each operation:
+
+// If it's a number, I convert it and push it to the stack.
+
+// If it's ‘+’, I calculate the sum of the last two scores and push it.
+
+// If it's ‘D’, I push double the previous score.
+
+// If it's ‘C’, I remove the last score.
+
+// After processing all operations, I sum up all the values in the stack and return the final score.
+// Using a stack makes it easy to access and manipulate the recent scores.”
