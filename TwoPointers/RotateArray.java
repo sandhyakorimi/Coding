e4c9acce13755f6ce0ca4b5002ed61ea7e2ac88a
SANDHYA <24P31F00D4@acet.ac.in>
2026-01-28 11:58:11 +0530

    rotate array

---
+++ TwoPointers/RotateArray.java
@@ -0,0 +1,17 @@
+package TwoPointers;
+public class RotateArray {
+    public void Rotate_an_Array(int nums[], int k){
+        int n=nums.length;
+        k%=n;
+        reverse(nums,0,n-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,n-1);
+    }
+    public void reverse(int nums[], int l, int r){
+        int temp = nums[l];
+        nums[l]=nums[r];
+        nums[r]=temp;
+        l++;
+        r--;
+    }
+}
