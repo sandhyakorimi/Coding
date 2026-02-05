925a82e3c24c942a393adcdef7817b259602697f
SANDHYA <24P31F00D4@acet.ac.in>
2026-02-05 21:25:01 +0530

    Path finding

---
+++ Stacks/PathFinding.java
@@ -0,0 +1,17 @@
+import java.util.Stack;
+
+class PathFinding {
+    public String simplifyPath(String path) {
+        Stack<String> stack = new Stack<>();
+        String[] paths = path.split("/");
+        for(String s : paths){
+            if(s.equals("..")){
+                if(!stack.isEmpty()) stack.pop();
+            }
+            else if(!s.equals("")&&!s.equals(".")){
+                stack.push(s);
+            }
+        }
+        return "/"+String.join("/",stack);
+    }
+}
